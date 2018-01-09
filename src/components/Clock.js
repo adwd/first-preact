92f7448b0c52bd70c9e67b9a9c116850ba0c0486
adwd <ecdt.mn@gmail.com>
2018-01-09 23:30:38 +0900

    add scss

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,11 +1,12 @@
 import { h, render, Component } from 'preact'
+import styles from './Clock.scss'
 
 export default class Clock extends Component {
   render({ date }) {
     return (
       <div>
         <p>time is</p>
-        <span>{date.toLocaleTimeString()}</span>
+        <span class={styles.clock}>{date.toLocaleTimeString()}</span>
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
+const ENV = process.env.NODE_ENV || 'development'
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  disable: ENV === 'development',
+})
 
 module.exports = {
   entry: './src/index.js',
@@ -13,12 +21,36 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        }),
+      },
     ],
   },
+  plugins: [
+    extractSass,
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      minify: { collapseWhitespace: true },
+    }),
+  ],
   devtool: 'source-map',
   devServer: {
     port: process.env.PORT || 8080,
     host: '0.0.0.0',
-    contentBase: path.join(__dirname, "build")
+    contentBase: path.join(__dirname, 'build'),
   },
 }
