669d152fa8e01ac7d7d091c0138f01f451cf0de6
adwd <adwd@users.noreply.github.com>
2018-01-09 14:31:33 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: process.env.PORT || 8080,
+    contentBase: './src',
+  },
+}
